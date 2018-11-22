9a3f0f7bfc85900d83bf8be318e7fd5584b4f017
gudulongao <gudulongao@qq.com>
2018-11-22 15:55:18 +0800

    增加红黑树的测试代码

--- src/main/test/demo/tree/redblacktree/RedBlackTreeTest.java
+++ src/main/test/demo/tree/redblacktree/RedBlackTreeTest.java
@@ -1,5 +1,29 @@
-import static org.junit.Assert.*;
+package demo.tree.redblacktree;
+
+import org.junit.After;
+import org.junit.Before;
 
 public class RedBlackTreeTest {
+    private RedBlackTree<Integer> tree;
 
+    @Before
+    public void init() throws Exception {
+        tree = new RedBlackTree<Integer>();
+        Integer[] datas = new Integer[]{9, -1, 24, 3, 6, 7, 12, 2, 4, 32, 0, 5, 1};
+        for (Integer data : datas) {
+            tree.insert(data);
+        }
+    }
+
+    @After
+    public void print() throws Exception {
+        tree.preIterator();
+        System.out.println();
+        tree.midIterator();
+        System.out.println();
+        tree.postIterator();
+        System.out.println();
+        tree.levelIterator();
+        System.out.println();
+    }
 }
